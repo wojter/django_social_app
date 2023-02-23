5789fa9d523f94708d6c9351e5c97254dde76185
wojter <wojtektro2@gmail.com>
2023-02-23 13:35:48 +0100

    Signup new user, create profile

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 def index(request):
@@ -25,6 +26,14 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                #log user in  and redirect to settings page
+
+                #create a Prfile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
