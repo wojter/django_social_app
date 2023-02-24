975555ab29ff5d763a064314df9b174d2062a936
wojter <wojtektro2@gmail.com>
2023-02-24 21:40:03 +0100

    Add upload post

--- core/views.py
+++ core/views.py
@@ -2,13 +2,33 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        
+        return redirect('/')
+    else:
+        return HttpResponse('<h1>Upload View</h1>')
+
 
 @login_required(login_url='signin')
 def settings(request):
@@ -24,7 +44,7 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-        
+
         if request.FILES.get('image') != None:
             image = request.FILES.get('image')
             bio = request.POST['bio']
@@ -38,8 +58,9 @@ def settings(request):
     else:
         return render(request, 'setting.html', {'user_profile': user_profile})
 
+
 def signup(request):
-    
+
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
@@ -52,18 +73,21 @@ def signup(request):
                 return redirect('signup')
             elif User.objects.filter(username=username).exists():
                 messages.info(request, 'Username taken')
-                return redirect('signup') 
+                return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
                 user.save()
 
-                #log user in  and redirect to settings page
-                user_login = auth.authenticate(username=username, password=password)
+                # log user in  and redirect to settings page
+                user_login = auth.authenticate(
+                    username=username, password=password)
                 auth.login(request, user_login)
 
-                #create a Prfile object for the new user
+                # create a Prfile object for the new user
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
                 new_profile.save()
                 return redirect('settings')
         else:
@@ -72,6 +96,7 @@ def signup(request):
 
     else:
         return render(request, 'signup.html')
+
 
 def signin(request):
 
@@ -89,6 +114,7 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
+
 
 @login_required(login_url='signin')
 def logout(request):
